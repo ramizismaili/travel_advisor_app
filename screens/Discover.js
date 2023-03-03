a92010d15044c430b36841d8d8a9ff51fe1edbdc
Ramiz Ismaili <rismaili49@gmail.com>
2023-03-03 05:50:16 +0100

    Discover Screen & Api Data Integration

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,12 +1,170 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import React, { useLayoutEffect, useState, useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import avatar from "../assets/avatar.png";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import MenuContainer from "../components/MenuContainer";
+import hotel from "../assets/hotel.png";
+import restaurants from "../assets/restaurants.png";
+import attraction from "../assets/attraction.png";
+import { FontAwesome } from "@expo/vector-icons";
+import ItemCardContainer from "../components/ItemCardContainer";
+import NotFound from "../assets/NotFound.png";
+import { getPlacesData } from "../api";
 
 const Discover = () => {
+  const navigation = useNavigation();
+
+  const [type, setType] = useState("restaurants");
+  const [isLoading, setIsLoading] = useState(false);
+  const [mainData, setMainData] = useState([]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions(
+      {
+        headerShown: false,
+      },
+      []
+    );
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData().then((data) => {
+      setMainData(data);
+      
+      setIsLoading(false);
+    });
+  }, []);
+
+  console.log(mainData);
+
   return (
-    <View>
-      <Text>Discover</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row items-center justify-between px-8">
+        <View>
+          <Text className="text-[40px] text-[#0B646B] font-bold">Discover</Text>
+          <Text className="text-[#527873] text-[36px]">the beauty today</Text>
+        </View>
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center">
+          <Image
+            source={avatar}
+            className="w-full h-full rounded-md object-cover"
+          />
+        </View>
+      </View>
+
+      <View className="flex-row items-center bg-white mx-4 rounded-xl py-1 px-4 shadow-lg mt-4">
+        {/* Search bar */}
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          fetchDetails={true}
+          GooglePlacesDetailsQuery={{ fields: "geometry" }}
+          onPress={(details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(details?.geometry?.viewport);
+          }}
+          query={{
+            key: "AIzaSyAfhneISUNBVe7BUJTatKJ4UFwiuif2834",
+            language: "en",
+          }}
+        />
+      </View>
+
+      {/* Menu Container */}
+      {isLoading ? (
+        <View className="flex-1 items-center justify-center">
+          <ActivityIndicator size="large" color="#0B646B" />
+        </View>
+      ) : (
+        <ScrollView>
+          <View className="flex-row items-center justify-between px-8 mt-8">
+            <MenuContainer
+              key={"hotels"}
+              title="Hotels"
+              imageSrc={hotel}
+              type={type}
+              setType={setType}
+            />
+            <MenuContainer
+              key={"attractions"}
+              title="Attractions"
+              imageSrc={attraction}
+              type={type}
+              setType={setType}
+            />
+            <MenuContainer
+              key={"restaurants"}
+              title="Restaurants"
+              imageSrc={restaurants}
+              type={type}
+              setType={setType}
+            />
+          </View>
+          {/* Top Tips */}
+          <View>
+            <View className="flex-row items-center justify-between px-4 mt-8">
+              <Text className="text-[#2C7379] text-[28px] font-bold">
+                Top Tips
+              </Text>
+              <TouchableOpacity className="flex-row items-center justify-center space-x-2">
+                <Text className="text-[#A0C4C7] text-[23px] font-bold">
+                  Explore
+                </Text>
+                <FontAwesome
+                  name="long-arrow-right"
+                  size={24}
+                  color="#A0C4C7"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
+              {mainData.length > 0 ? (
+                <>
+                  {mainData?.map((data, i) => (
+                    <ItemCardContainer
+                      key={i}
+                      imageSrc={
+                        data?.photo?.images?.medium?.url
+                          ? data?.photo?.images?.medium?.url
+                          : "https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"
+                      }
+                      title={data?.name}
+                      location={data?.location_string}
+                      data={data}
+                    />
+                  ))}
+                </>
+              ) : (
+                <>
+                  <View className="w-full h-[400px] items-center space-y-8 justify-center">
+                    <Image
+                      source={NotFound}
+                      className=" w-32 h-32 object-cover"
+                    />
+                    <Text className="text-2xl text-[#428288] font-semibold">
+                      Opps...No Data Found
+                    </Text>
+                  </View>
+                </>
+              )}
+            </View>
+          </View>
+        </ScrollView>
+      )}
+    </SafeAreaView>
   );
 };
+
+// {"northeast": {"lat": 25.41257834546226, "lng": 51.62812119686502}, "southwest": {"lat": 25.15543993776612, "lng": 51.39587210719369}}
 
 export default Discover;
